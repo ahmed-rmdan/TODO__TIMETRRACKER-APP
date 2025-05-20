6c9555df16699838276b71b0aac209486056f991
ahmed-rmdan <ahmedrmadan251998@gmail.com>
2025-05-20 10:54:46 +0300

    finish todo class

--- TODO-APP/src/main.ts
+++ TODO-APP/src/main.ts
@@ -1,1 +1,15 @@
 import './style.css'
+const submitbutton=document.querySelector('.submit')
+const timevalue=document.querySelector<HTMLInputElement>('.time')
+const datevalue=document.querySelector<HTMLInputElement>('.date')
+console.log(submitbutton)
+submitbutton?.addEventListener('click',ev=>{
+    ev.preventDefault();
+        const hours=timevalue?.value
+        console.log(hours)
+    // const date=datevalue?.valueAsDate
+    // date?.setHours(10)
+    //  console.log(date?.getHours())
+         
+
+})
--- TODO-APP/src/todo.ts
+++ TODO-APP/src/todo.ts
@@ -3,10 +3,19 @@ class todo{
     id:string;
     task:string;
     date:Date;
-    constructor(task:string){
+    time:string;
+    constructor(task:string,date:Date,time:string){
       this.task=task;
       this.id=new Date().toString();
-      this.date=new Date()
+      this.date=date
+      this.time=time
+      this.sethours()
+    }
+    sethours(){
+    const hours:number= Number(this.time.split(':')[0])
+    const minutes:number=Number(this.time.split(':')[1])
+    this.date.setHours(hours)
+    this.date.setMinutes(minutes)
     }
 
 }
