323dfb8871d7346ca06576e7b9983cd2b07b4b5e
ahmed-rmdan <ahmedrmadan251998@gmail.com>
2025-05-21 17:24:51 +0300

    finish handle buttuns

--- TODO-APP/src/main.ts
+++ TODO-APP/src/main.ts
@@ -6,6 +6,9 @@ const submitbutton=document.querySelector<HTMLButtonElement>('.submit')
 const taskvalue=document.querySelector<HTMLInputElement>('.task')
 const timevalue=document.querySelector<HTMLInputElement>('.time')
 const datevalue=document.querySelector<HTMLInputElement>('.date')
+const nextbutton=document.querySelector<HTMLButtonElement>('.next button')
+const prevbutton=document.querySelector<HTMLButtonElement>('.prev button')
+
 
 submitbutton?.addEventListener('click',ev=>{
     ev.preventDefault();
@@ -16,11 +19,25 @@ if(taskvalue?.value===''||timevalue===undefined||datevalue===undefined){
 else{
  const setdate=new todo(taskvalue?.value as string,datevalue?.valueAsDate as Date,timevalue?.value as string)
         modalview.pushtask(setdate)
-        modalview.updatepagemodal()
-        const viewtasks=new Viewtasks(modal.todos)
+        const viewtasks=new Viewtasks(modal.pagetodo)
         viewtasks.render()      
 }
                
 })
 
+nextbutton?.addEventListener('click',ev=>{
+    ev.preventDefault()
+   modalview.handlenext()
+   const viewtasks=new Viewtasks(modal.pagetodo)
+   viewtasks.render()
+})
 
+
+prevbutton?.addEventListener('click',ev=>{
+    ev.preventDefault()
+   modalview.handleprev()
+   const viewtasks=new Viewtasks(modal.pagetodo)
+   viewtasks.render()
+})
+const x=[1,2,3,4,5,6]
+console.log(x.splice(0))
--- TODO-APP/src/modal.ts
+++ TODO-APP/src/modal.ts
@@ -1,52 +1,83 @@
 import { todo } from "./todo"
 
-export const modal:{todos:todo[],length:number,start:number,end:number,pages:number}={
+export const modal:{todos:todo[],length:number,start:number,end:number,maxpages:number,activepage:number,pagetodo:todo[]}={
 todos:[],
 length:0,
 start:0,
 end:3,
-pages:1
+maxpages:1,
+activepage:1,
+pagetodo:[]
 }
 function pushtask(todo:todo){
     modal.todos.push(todo)
-    modal.length++;
-    if(modal.length===0){
-        modal.pages=1
-    }
-    else{
-modal.pages=Number((modal.length/3).toFixed())
-    }
-    
+    modal.length++;  
+   modal.maxpages=Math.ceil(modal.length/3) 
+  updatepushitems()
+
 }
 function removetask(id:string){
 modal.todos.filter(to=>
     to.id!==id
 )
  length--;   
- modal.pages=Number((modal.length/3).toFixed())
+
 
 }
-function updatepagemodal(){
-    
-  modal.todos=modal.todos.slice(modal.start,modal.end)
-}
+
 function handlenext(){
+if(modal.activepage<modal.maxpages){
+    updatenextitems()
+    modal.activepage=modal.activepage+1
+}else{
+    return
+}
 
-    modal.start=modal.start+3;
-    modal.end=modal.end+3;
-    modalview.updatepagemodal()
-
+    
 }
 function handleprev(){
-    modal.start=modal.start-3;
-    modal.end=modal.end-3;
-    modalview.updatepagemodal()
+    console.log(modal.activepage)
+if(modal.activepage===1){
+    return;
+}else{
+     modal.activepage=modal.activepage-1;
+    updateprevitems()
+   
+}
+}
+function updatepushitems(){
+    if(modal.activepage===modal.maxpages){
+      let  newtodos=[...modal.todos]
+      const start=(modal.activepage-1)*3
+      const end=modal.length
+      
+     modal.pagetodo=newtodos.slice(start,end)
+      }
+      else{
+     return;
+      }
+ 
+}
+function updatenextitems(){
+     let  newtodos=[...modal.todos]
+    const start=modal.activepage*3
+    const end=start+3
+    modal.pagetodo=newtodos.slice(start,end)
+}
+function updateprevitems(){
+      let  newtodos=[...modal.todos]
+      console.log(newtodos)
+     
+    const end=modal.activepage*3
+    const start=end-3
+     console.log(start)
+      console.log(end)
+    modal.pagetodo=newtodos.slice(start,end)
 }
 
 export const modalview={
     pushtask,
     removetask,
-    updatepagemodal,
     handlenext,
     handleprev
 
